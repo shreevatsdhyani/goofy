3d44bbb770865ed144a98f28ee1ac66d18540435
Shreevats Dhyani <shreevats37@gmail.com>
2024-04-03 20:21:58 +0530

    hello

---
+++ main.js
@@ -0,0 +1,22 @@
+let expanded = true;
+function lnav() {
+    const navelements = ["home", "explore", "library", "addartists", "newplaylist", "hrnav"];
+    if (expanded) {
+        document.getElementById("leftnav").classList.replace("w-[240px]","w-[72px]");
+        for (const i of navelements) {
+            document.getElementById(i).classList.add("hidden");
+        }
+        expanded = false;
+    } 
+    else {
+        document.getElementById("leftnav").classList.replace("w-[72px]","w-[240px]");
+        for (const i of navelements) {
+
+            document.getElementById(i).classList.remove("hidden");
+    
+        }
+        expanded = true;
+    }
+    
+
+}
