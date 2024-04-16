520c67d7e5f7c4fe23cfe8cfd3e988a2cbcfdaab
warui_slime <rohansaini456@gmail.com>
2024-04-16 15:57:33 +0530

    15:57 16-04-2024 @warui-slime

--- searcheng.py
+++ searcheng.py
@@ -1,6 +1,8 @@
 import warnings
 import httpx
 import re
+from bs4 import BeautifulSoup
+import requests
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -11,7 +13,7 @@ def search(query: str, num_tracks: int = 5) -> list[dict[str, str]]:
 
     page = httpx.get(f"https://music.youtube.com/search?q={query}", headers=headers).content.decode("unicode_escape")
 
-    track_ids = list(set(re.findall(r'"videoId":"(.*?)"', page)))[:num_tracks]
+    track_ids = list(set(re.findall(r'"videoId":"(.*?)"', page)))[3:num_tracks+3]
 
     track_info_list = []
 
@@ -29,3 +31,22 @@ def search(query: str, num_tracks: int = 5) -> list[dict[str, str]]:
         track_info_list.append(track_data)
 
     return track_info_list
+
+# def ultrapromaxscrapping():
+# headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+#     }
+# page = httpx.get(f"https://music.youtube.com/search?q=shape", headers=headers)
+
+# with open("something.json","w",encoding="unicode_escape") as  f:
+#     f.write(BeautifulSoup(page,"html5lib").prettify())
+# p = requests.get("https://music.youtube.com/search?q=shape",headers)
+# x = BeautifulSoup(page.content.decode("unicode_escape"),"html.parser")
+# q=x.findAll("div",{"class":"style-scope ytmusic-shelf-renderer"})
+# print(q)
+# print(x.prettify().find("contents"))
+# with open("something.html","w",encoding="unicode_escape") as  f:
+#     f.write(bytes(x.prettify(),"utf-8").decode("unicode_escape").replace("\\",""))
+# with open("something.html","w+") as  f:
+#     x = bytes(f.read().encode()).decode("unicode_escape")
+#     f.write(x)
