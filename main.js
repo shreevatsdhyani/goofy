0b43233554e98be72f85fd06ee4c6ef00699e000
warui_slime <rohansaini456@gmail.com>
2024-05-04 15:54:24 +0530

    15:54 04-05-2024 @warui-slime

--- main.js
+++ main.js
@@ -84,42 +84,13 @@ function clearinput() {
     crossappear();
 }
 
-function heading1material() {
-    const songscroll = document.getElementById("parentscrolldiv");
-    const elementstr = `<div>
-    <div class="h-40 w-40">
-        <img src="assets/songthumbnail.svg" alt="">
-    </div>
-    <div class="text-white w-40 pt-1">
-        Song Name
-    </div>
-    <div class="text-white w-40">
-        Artist Name
-    </div>
-</div>`
-    const parser = new DOMParser();
-    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
-    for (let i = 0; i < 8; i++) {
-        songscroll.appendChild(newelements.cloneNode(true));
-    }
-}
-function createribbon() {
-    const genres = ["Relax", "Romance", "Energize", "Party", "Workout", "Sad", "Focus", "Sleep"];
-    const ribbox = document.getElementById("ribbonbox");
-    const parser = new DOMParser();
-    genres.forEach(ele => {
-        ribbox.appendChild(parser.parseFromString(`<div>
-        <button class="ribbonbuttons">
-            <RB class="p-3">${ele}</RB>
-        </button>
-    </div>`, "text/html").body.firstChild);
-    });
-}
-function horizontalscroll(left) {
+
+
+function horizontalscroll(left,eleid) {
     if (left) {
-        document.getElementById("parentscrolldiv").scrollLeft -= 200;
+        document.getElementById(eleid).scrollLeft -= 200;
     } else {
-        document.getElementById("parentscrolldiv").scrollLeft += 200;
+        document.getElementById(eleid).scrollLeft += 200;
     }
 
 }
@@ -258,8 +229,7 @@ document.getElementById("likedsongsbox").addEventListener("mouseenter", () => {
 document.getElementById("likedsongsbox").addEventListener("mouseleave", () => {
     document.getElementById("likedsongsimg").classList.remove("animate-pulse");
 });
-heading1material();
-createribbon();
+
 
 
 function volumeManage() {
