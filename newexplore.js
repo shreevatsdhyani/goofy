df3521fb0f1608edeacdcc129534b207441634cb
Shreevats Dhyani <shreevats37@gmail.com>
2024-07-02 20:04:29 +0530

    @shreevatsdhyani

--- newexplore.js
+++ newexplore.js
@@ -24,7 +24,7 @@ function heading1material() {
     const songscroll = document.getElementById("parentscrolldiv1");
     const elementstr = `<div>
     <div class="h-40 w-40 border-white border-2">
-        <img src="assets/songthumbnail.svg" alt="">
+        <img src="newassets/songdp.svg" alt="">
     </div>
     <div class="text-white w-40 pt-1">
         Song Name
@@ -43,7 +43,43 @@ function heading2material() {
     const songscroll = document.getElementById("parentscrolldiv2");
     const elementstr = `<div>
     <div class="h-40 w-40 border-white border-2">
-        <img src="assets/songthumbnail.svg" alt="">
+        <img src="newassets/songdp.svg" alt="">
+    </div>
+    <div class="text-white w-40 pt-1">
+        Song Name
+    </div>
+    <div class="text-white w-40">
+        Artist Name
+    </div>
+</div>`
+    const parser = new DOMParser();
+    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
+    for (let i = 0; i < 10; i++) {
+        songscroll.appendChild(newelements.cloneNode(true));
+    }
+}
+
+
+function moodandgenres() {
+    const moods = ["Romantic","Melancholic","Happy","Energetic","Peaceful","Spiritual","Patriotic","Festive","Rainy Day","Wedding","Workout","Meditation","Travel","Chill","Motivational","Party","Soulful","Retro","Urban","Rural","Nostalgic","Contemporary","Experimental","Unwind","Love","Dance","Lounge","Study"];
+    const mandg = document.getElementById("mandg");
+    const parser = new DOMParser();
+    moods.forEach(ele =>{
+        mandg.appendChild(parser.parseFromString(`<div class="hover:bg-[#111827]">
+        <button class="mandg">
+            <MG class="p-3">${ele}</MG>
+        </button>
+    </div>`,"text/html").body.firstChild);
+    });
+}
+
+
+
+function heading4material() {
+    const songscroll = document.getElementById("parentscrolldiv4");
+    const elementstr = `<div>
+    <div class="h-40 w-40 border-white border-2">
+        <img src="newassets/artistdp.svg" alt="">
     </div>
     <div class="text-white w-40 pt-1">
         Song Name
@@ -72,3 +108,5 @@ document.getElementById("explorebox").addEventListener("mouseleave", () => {
 createribbon();
 heading1material();
 heading2material();
+heading4material();
+moodandgenres();
