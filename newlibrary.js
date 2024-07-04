d5bf8cfe2649c2b24effd4d3e5e8023257380a09
Shreevats Dhyani <shreevats37@gmail.com>
2024-07-04 18:03:44 +0530

    @shreevatsdhyani

--- newlibrary.js
+++ newlibrary.js
@@ -1,65 +1,17 @@
-
-function playlistCreate() {
-    const songscroll = document.getElementById("playlistscrolldiv");
-    const elementstr1 = `
-    <div>
-        <button>
-                <img class="border-4" src="newassets/songdp.svg" alt="">
+function createribbon() {
+    const genres = { "Songs": "bg-[#FF5A5A]", "Albumns": "bg-[#FF75C5]", "Artists": "bg-[#8FFF7D]", "Playlists": "bg-[#76DEFF]" };
+    const ribbox = document.getElementById("ribbonbox");
+    const parser = new DOMParser();
+    Object.entries(genres).forEach(([ele, k]) => {
+        ribbox.appendChild(parser.parseFromString(`<div>
+        <button class="${k} mr-2 text-black text-lg font-semibold border-white border-[3px] rounded-lg hover:shadow-lg hover:shadow-white hover:scale-110">
+            <div class="px-5 py-1 text-xl">${ele}</div>
         </button>
-        <div class="text-white text-start">
-            Song Name
-        </div>
-        <div class="text-white text-start">
-            Artist Name
-        </div>    
-        
-    </div>
-                   
-
-`
-    const elementstr2 = `
-    <div> 
-        <div>
-            <button data-modal-target="crud-modal" data-modal-toggle="crud-modal">
-                <div>
-                    <div class="bg-black h-[188px] w-[188px] border-4 pt-12 pl-14">
-                        <img  src="newassets/createplaylist.svg" alt=""></div>
-                    </div>
-                </div>    
-            </button>
-            <div class="text-white text-start">
-            Create Playlist
-        </div>
-        </div>
-    </div>`
-    const parser = new DOMParser();
-    // const createpl = parser.parseFromString(elementstr2, "text/html").body.firstChild;
-    for (let i = 0; i < 1; i++) {
-        songscroll.appendChild(parser.parseFromString(elementstr1,"text/html").body.firstChild);
-    }
-    songscroll.appendChild(parser.parseFromString(elementstr2,"text/html").body.firstChild);
-    
+    </div>`, "text/html").body.firstChild);
+    });
 }
 
-
-function popularpl() {
-    const songscroll = document.getElementById("popularscrolldiv");
-    const elementstr = `<div>
-    <div>
-        <img class="border-4" src="newassets/songdp.svg" alt="">
-    </div>
-    <div class="text-white w-40 pt-1">
-        Playlist Name
-    </div>
-    
-</div>`
-    const parser = new DOMParser();
-    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
-    for (let i = 0; i < 8; i++) {
-        songscroll.appendChild(newelements.cloneNode(true));
-    }
-}
-function horizontalscroll(left,eleid) {
+function horizontalscroll(left, eleid) {
     if (left) {
         document.getElementById(eleid).scrollLeft -= 200;
     } else {
@@ -68,8 +20,97 @@ function horizontalscroll(left,eleid) {
 
 }
 
-playlistCreate();
-popularpl();
+function heading1material() {
+    const songscroll = document.getElementById("parentscrolldiv1");
+    const elementstr = `
+    
+    
+    <div>
+    <div class="h-40 w-40 border-white border-2">
+        <img src="newassets/songdp.svg" alt="">
+    </div>
+    <div class="text-white w-40 pt-1">
+        Song Name
+    </div>
+    <div class="text-white w-40">
+        Artist Name
+    </div>
+</div>`
+    const parser = new DOMParser();
+    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
+    for (let i = 0; i < 10; i++) {
+        songscroll.appendChild(newelements.cloneNode(true));
+    }
+}
+function heading2material() {
+    const songscroll = document.getElementById("parentscrolldiv2");
+    const elementstr = `
+    
+    
+    <div>
+    <div class="h-40 w-40 border-white border-2">
+        <img src="newassets/songdp.svg" alt="">
+    </div>
+    <div class="text-white w-40 pt-1">
+        Song Name
+    </div>
+    <div class="text-white w-40">
+        Artist Name
+    </div>
+</div>`
+    const parser = new DOMParser();
+    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
+    for (let i = 0; i < 10; i++) {
+        songscroll.appendChild(newelements.cloneNode(true));
+    }
+}
+function heading3material() {
+    const songscroll = document.getElementById("parentscrolldiv3");
+    const elementstr = `
+    <div>
+    <div class="h-40 w-40 border-white border-2">
+        <img src="newassets/songdp.svg" alt="">
+    </div>
+    <div class="text-white w-40 pt-1">
+        Song Name
+    </div>
+    <div class="text-white w-40">
+        Artist Name
+    </div>
+</div>`
+    const parser = new DOMParser();
+    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
+    for (let i = 0; i < 10; i++) {
+        songscroll.appendChild(newelements.cloneNode(true));
+    }
+}
+function heading4material() {
+    const songscroll = document.getElementById("parentscrolldiv4");
+    const elementstr = `
+    <div>
+    <div class="h-40 w-40 border-white border-2">
+        <img src="newassets/songdp.svg" alt="">
+    </div>
+    <div class="text-white w-40 pt-1">
+        Playlist Name
+    </div>
+    
+</div>`
+    const parser = new DOMParser();
+    const newelements = parser.parseFromString(elementstr, "text/html").body.firstChild;
+    for (let i = 0; i < 1; i++) {
+        songscroll.appendChild(newelements.cloneNode(true));
+    }
+}
+
+
+document.getElementById("explorebox").addEventListener("mouseenter", () => {
+    document.getElementById("exploreimg").classList.add("animate-spin");
+});
+document.getElementById("explorebox").addEventListener("mouseleave", () => {
+    document.getElementById("exploreimg").classList.remove("animate-spin");
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
     const modalToggle = document.querySelector('[data-modal-toggle="crud-modal"]');
@@ -77,25 +118,37 @@ document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('crud-modal');
 
     modalToggle.addEventListener('click', function () {
-      modal.classList.toggle('hidden');
+        modal.classList.toggle('hidden');
     });
 
     modalClose.addEventListener('click', function () {
-      modal.classList.toggle('hidden');
+        modal.classList.toggle('hidden');
     });
-  });
+});
 
-  function createribbon() {
-    const genres = {"Song":"[#7D2C2C]", "Albumns":"[#FAFF00]", "Artists":"[#38771A]", "Playlists":"[#28739D]"};
-    const ribbox = document.getElementById("ribbonbox");
-    const parser = new DOMParser();
-    Object.entries(genres).forEach(([ele,k]) => {
-        ribbox.appendChild(parser.parseFromString(`<div>
-        <button class="bg-${k} mr-2 text-black text-lg font-semibold border-white border-[3px] rounded-lg">
-            <div class="px-5 py-1 text-xl text-shadow-lg ">${ele}</div>
-        </button>
-    </div>`, "text/html").body.firstChild);
-    });
-}
+
+// // Wait for the DOM content to load
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get the modal and the toggle button
+//     var modal = document.getElementById('crud-modal');
+//     var modalToggle = document.querySelector('[data-modal-toggle="crud-modal"]');
+
+//     // Add event listener for clicks on the document
+//     document.addEventListener('click', function(event) {
+//         // Check if the click is outside the modal and not on the toggle button
+//         var isClickInsideModal = modal.contains(event.target);
+//         var isClickOnToggle = (event.target === modalToggle);
+
+//         if (!isClickInsideModal && !isClickOnToggle) {
+//             // If clicked outside modal and not on toggle, hide the modal
+//             modal.classList.add('hidden');
+//         }
+//     });
+// });
+
 
 createribbon();
+heading1material();
+heading2material();
+heading3material();
+heading4material();
